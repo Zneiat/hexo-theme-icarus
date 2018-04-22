6199a0d9495fb03813ded3383f1835adf940e1ff
zneiat <1149527164@qq.com>
2018-04-22 22:19:17 +0800

    修改了一下样式

--- scripts/thumbnail.js
+++ scripts/thumbnail.js
@@ -5,5 +5,13 @@
 *     <%- thumbnail(post) %>
 */
 hexo.extend.helper.register('thumbnail', function (post) {
-    return post.thumbnail || post.banner || '';
+    const asset_img = hexo.extend.helper.get('asset_img').bind(this);
+
+    if (!!post.thumbnail)
+        return post.thumbnail;
+
+    if (!!post.banner)
+        return asset_img(post, post.banner);
+
+    return '';
 });
