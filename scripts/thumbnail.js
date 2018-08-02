d69cff3acb1d40a1c401f0ff2355082d9e0d273e
qwqcode <1149527164@qq.com>
2018-08-02 11:43:28 +0800

    日常更新

--- scripts/thumbnail.js
+++ scripts/thumbnail.js
@@ -5,6 +5,9 @@
 *     <%- thumbnail(post) %>
 */
 hexo.extend.helper.register('thumbnail', function (post) {
+    if (!hexo.theme.config.post.banner_use_asset_img)
+        return post.thumbnail || post.banner || '';
+
     const asset_img = hexo.extend.helper.get('asset_img').bind(this);
 
     if (!!post.thumbnail)
